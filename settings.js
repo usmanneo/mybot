59d31a609aa759d703179621773e8156c0d922a9
usmanneo <h4k3rtools@gmail.com>
2024-05-29 15:07:12 +0500

    settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.botname = 'Usman Bot MD V11' //ur bot name
+global.ownernumber = '923034088220' //ur owner number
+global.ownername = '🦄Usman Pasha' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
 global.themeemoji = '🪀'
@@ -31,7 +31,7 @@ global.packname = "Sticker By"
 global.author = "🦄드림 가이 Xeon\n\n+916909137213"
 global.creator = "916909137213@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["923034088220"] // Premium User
 global.hituet = 0
 
 //bot sett
